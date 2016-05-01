6d6ba6b87bdba9d10eeaf6a0ff3d283893e97bba
William Rudenmalm <me@whn.se>
2016-05-01 21:36:05 +0200

    use chrome transport in chrome

--- index.js
+++ index.js
@@ -4,6 +4,14 @@
 const kad = require("kad")
 const connect = require("./lib/network.js")
 
+function selectTransport() {
+  if (chrome) {
+    return require("kad-transport-chrome-udp")
+  } else {
+    return kad.transports.UDP
+  }
+}
+
 const me = {
   address: "127.0.0.1",
   port: parseInt(process.argv[2]),
@@ -14,10 +22,9 @@ const seed = {
   port: parseInt(process.argv[3]),
 }
 
-const storage = new kad.storage.MemStore(),
-      transport = kad.transports.UDP
+const storage = new kad.storage.MemStore()
 
-connect(me, seed, transport, storage).then(dht => {
+connect(me, seed, selectTransport(), storage).then(dht => {
   console.log("foo")
   dht.put("test", "x", () => {
     dht.get("test", (err, val) => {
